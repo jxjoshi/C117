fb4a18d6bc1cd3049b57199d94504d16c2f3df1c
Jai Joshi <83172501+jxjoshi@users.noreply.github.com>
2022-12-04 10:53:05 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, url_for, request, jsonify
+from text_sentiment_prediction import *
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+@app.route('/predict-emotion', methods=["POST"])
+def predict_emotion():
+    
+    # Get Input Text from POST Request
+    input_text=request.json.get("text")
+    if not input_text:
+        response={
+            "status":"error",
+            "message":"Please enter some text to predict the emotion."
+        }
+        return jsonify(response)
+    else:
+        predicted_emotion, predicter_emotion_image_url = 
+   
+    
+    if not input_text:
+        # Response to send if the input_text is undefined
+       
+        
+        # Response to send if the input_text is not undefined
+        
+        # Send Response         
+        
+       
+app.run(debug=True)
+
+
+
+    
